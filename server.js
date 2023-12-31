45d7882ea1cbad90df806acf4763820173efb499
Brynner03 <bventura03@hotmail.com>
2023-12-31 17:36:26 -0500

    Redirect users from localhost to the deployed backend.

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ app.post("/checkout", async (req, res, next) => {
             quantity: item.quantity,
           })),
            mode: "payment",
-           success_url: "http://localhost:4242/success.html",
-           cancel_url: "http://localhost:4242/cancel.html",
+           success_url: "http://https://merch-store-api.onrender.com/success.html",
+           cancel_url: "http://https://merch-store-api.onrender.com/cancel.html",
         });
 
         res.status(200).json(session);
